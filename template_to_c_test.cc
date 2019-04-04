bf77bb2f3437c343648c5ba10ac43ecc9a7a886d
Phillip F Knaack <pknaack1@gmail.com>
2019-04-04 11:03:19 -0500

    dont let template_to_c_test overwrite compiled binary used by makefile

--- template_to_c_test.cc
+++ template_to_c_test.cc
@@ -1,9 +1,10 @@
 #if 0
 # test this by typing "bash template_to_c_test.cc"
 set -e -x
+mkdir -p build_native
 cd build_native
-g++ -DECHO_OUTPUT=1 ../template_to_c.cc -o template_to_c
-./template_to_c ../template_to_c_test.cc template_test_out.cc template_test_out.h
+g++ -DECHO_OUTPUT=1 ../template_to_c.cc -o template_to_c_test
+./template_to_c_test ../template_to_c_test.cc template_test_out.cc template_test_out.h
 ls -l template_test_out.cc template_test_out.h
 g++ -I. -I.. ../template_to_c_test.cc ../template_patterns.cc template_test_out.cc -o template_test
 ./template_test
